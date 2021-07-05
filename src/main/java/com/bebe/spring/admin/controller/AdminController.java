f01747cadb50765dc5be2afc3b6449b31bfe7cec
hoogun <hoogun@DESKTOP-RURVHJP>
2021-07-05 19:22:45 +0900

    0705 19:22

---
+++ src/main/java/com/bebe/spring/admin/controller/AdminController.java
@@ -0,0 +1,6 @@
+package com.bebe.spring.admin.controller;
+
+public class AdminController {
+
+//	깃허브 테스트용 후건222222
+}
---
+++ src/main/webapp/static/js/aside.js
@@ -0,0 +1,5 @@
+// $('.menu').click(function() {
+//     var indexNo = $(this).index();
+//     $('.menu').css({ 'transform': 'none' });
+//     $(this).css({ 'transform': 'scale3d(1.2, 1.2, 0.3) translate(-7%, -5%)' })
+// })
