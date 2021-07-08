6be4c327c35c0520754cb1f8bb714ea26217ff2b
hoogun <hoogun@DESKTOP-RURVHJP>
2021-07-08 12:32:36 +0900

    210708 12:32

--- src/main/java/com/bebe/spring/login/controller/LoginController.java
+++ src/main/java/com/bebe/spring/login/controller/LoginController.java
@@ -1,11 +1,8 @@
 package com.bebe.spring.login.controller;
-
-import java.util.List;
-
+//오용욱
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
